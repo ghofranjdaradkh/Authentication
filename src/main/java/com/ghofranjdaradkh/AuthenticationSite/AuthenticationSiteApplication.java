b83a41f1c62de671e11aab6259ed11143edad52e
ghofranjdaradkh <ghofran_daradkh@hotmail.com>
2023-09-12 23:46:44 +0300

    authentication

---
+++ src/main/java/com/ghofranjdaradkh/AuthenticationSite/AuthenticationSiteApplication.java
@@ -0,0 +1,13 @@
+package com.ghofranjdaradkh.AuthenticationSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghofranjdaradkh/AuthenticationSite/AuthenticationSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghofranjdaradkh.AuthenticationSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
